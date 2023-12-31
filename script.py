d2660c53223c606882822b6a23b02a6c910a9b31
Brigham Andersen <brighambandersen@gmail.com>
2023-12-30 21:19:36 -0700

    All done with headless version

--- script.py
+++ script.py
@@ -1,35 +1,38 @@
 from json import loads
-from time import sleep
-import getpass
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from utils import calculate_tithing, float_to_currency
+from utils import required_input, calculate_tithing, float_to_currency
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import sys
+
+
+TIMEOUT = 10
 
 
 def main():
     print('\nWelcome to the Venmo tithing calculator!\n')
 
     print("Log in to your Venmo account")
-    venmo_email = input('Email:\t\t')
-    venmo_password = getpass.getpass('Password:\t')
+    venmo_email = required_input('Email:\t\t')
+    venmo_password = required_input('Password:\t', is_password=True)
 
     print("\nEnter what range to download (must be 2022 and after)")
-    start_date = input('Start date (i.e. 2022-12-25):\t')
-    end_date = input('End date (i.e. 2022-12-25):\t')
+    start_date = required_input('Start date (i.e. 2022-12-25):\t')
+    end_date = required_input('End date (i.e. 2022-12-25):\t')
 
-    print('\nGetting venmo transactions and calculating tithing (expect a 10 second wait)...\n')
+    print('\nGetting venmo transactions and calculating tithing (this will take serveral seconds)...\n')
 
     # Selenium - Login to access credentials for API
 
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless")
     driver = webdriver.Chrome(options=chrome_options)
 
     # Hit a simple venmo url to get an access token
     driver.get("https://account.venmo.com/settings/security")
-    sleep(2)
 
     email_input = driver.find_element(By.ID, 'email')
     email_input.send_keys(venmo_email)
@@ -39,16 +42,23 @@ def main():
     password_input.send_keys(venmo_password)
     password_input.send_keys(Keys.ENTER)
 
-    sleep(5)  # Wait for login to complete
+    try:
+        WebDriverWait(driver, TIMEOUT).until(
+            EC.text_to_be_present_in_element((By.TAG_NAME, "h1"), "Security")
+        )
+    except:
+        driver.quit()
+        print(
+            'Error occurred while logging in. Check your username and password and try again.')
+        sys.exit(1)
 
     # Now that you have credentials, get data from the API
-    transactions_url = f'https://account.venmo.com/api/statement/download?startDate={start_date}&endDate={end_date}'
-    driver.get(transactions_url)
-    sleep(2)
+    driver.get(
+        f'https://account.venmo.com/api/statement/download?startDate={start_date}&endDate={end_date}')
     json_string = driver.find_element(By.TAG_NAME, 'pre').text
     response = loads(json_string)
 
-    driver.close()
+    driver.quit()
 
     # Calculate tithing from json
 
--- utils.py
+++ utils.py
@@ -1,3 +1,15 @@
+from getpass import getpass
+
+
+def required_input(prompt: str, is_password=False) -> str:
+    while True:
+        user_input = getpass(prompt) if is_password else input(prompt)
+        if user_input:
+            return user_input
+        else:
+            print("Input is required. Try again.")
+
+
 def float_to_currency(amount: float) -> str:
     return f'${amount:.2f}'
 
