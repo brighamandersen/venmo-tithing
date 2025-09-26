5966270c007b0957438ae1219706f0f12f34dbbb
Brigham Andersen <brighambandersen@gmail.com>
2025-09-25 22:16:49 -0600

    Readjust structure

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,15 +3,20 @@ set -euo pipefail
 
 echo "Deploying venmo-tithing"
 
-sudo ln -sf /home/brig/code/venmo-tithing/deploy/systemd/venmo-tithing.service /etc/systemd/system/venmo-tithing.service
+# nginx
 
-sudo ln -sf /home/brig/code/venmo-tithing/deploy/nginx/venmo-tithing.conf /etc/nginx/conf.d/venmo-tithing.conf
+sudo ln -sf /home/brig/code/venmo-tithing/deploy/nginx.conf /etc/nginx/conf.d/venmo-tithing.conf
+
+sudo nginx -t
+sudo systemctl reload nginx
+
+# systemd
+
+sudo ln -sf /home/brig/code/venmo-tithing/deploy/systemd.service /etc/systemd/system/venmo-tithing.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable venmo-tithing.service
 sudo systemctl restart venmo-tithing.service
 
-sudo nginx -t
-sudo systemctl reload nginx
 
 echo "Deployment complete for venmo-tithing"
