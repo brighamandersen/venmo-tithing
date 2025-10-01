d5e82edb6104c35f6d0731f18be304b19358069c
Brigham Andersen <brighambandersen@gmail.com>
2025-10-01 04:28:40 +0000

    fix venmo tithing deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,18 +5,17 @@ echo "Deploying venmo-tithing"
 
 # nginx
 
-sudo ln -sf /home/brig/code/venmo-tithing/deploy/nginx.conf /etc/nginx/conf.d/venmo-tithing.conf
+sudo cp /home/brig/code/venmo-tithing/deploy/nginx.conf /etc/nginx/conf.d/venmo-tithing.conf
 
 sudo nginx -t
 sudo systemctl reload nginx
 
 # systemd
 
-sudo ln -sf /home/brig/code/venmo-tithing/deploy/systemd.service /etc/systemd/system/venmo-tithing.service
+sudo cp /home/brig/code/venmo-tithing/deploy/systemd.service /etc/systemd/system/venmo-tithing.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable venmo-tithing.service
 sudo systemctl restart venmo-tithing.service
-
 
 echo "Deployment complete for venmo-tithing"
